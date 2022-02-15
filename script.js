fc04126d16446fe71f6fc045978f11edcb6d561b
jstfleur <32300873+jstfleur@users.noreply.github.com>
2022-02-15 01:11:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,53 @@ const nav=document.querySelector("nav"),
       allSection=document.querySelectorAll(".section"),
       totalSection=allSection.length;
 
-for(let i=0; i<totalNavList; i++){
-    const a=navList[i];
-    a.addEventListener("click", function(){
-        var current = document.getElementsByClassName("active");
-        current[0].classList.remove("active");
-        this.classList.add("active");
+
+
+for(let i=0; i<totalNavList; i++) {
+    const a = navList[i];
+    a.addEventListener("click", function() {
+        //idk
+        removeActiveClass();
         
-        if (window.innerWidth < 1200){
-            
+        for (let j=0; j<totalNavList; j++){
+            if (navList[j].classList.contains("active")){
+//                addActiveClass(j);
+//                 allSection[j].classList.remove("active");
+                removeActiveClass();
+            }
+            navList[j].classList.remove("active");
         }
-        
+        this.classList.add("active");
+        showSection(this);
     })
 }
 
 
 
+//for(let i=0; i<totalNavList; i++){
+//    const a=navList[i];
+//    a.addEventListener("click", function(){
+//        var current = document.getElementsByClassName("active");
+//        current[0].classList.remove("active");
+//        this.classList.add("active");
+//        
+//        if (window.innerWidth < 1200){
+//            
+//        }
+//        
+//    })
+//
+//}
 
+function removeActiveClass(){
+    for(let i=0; i<totalSection; i++){
+        allSection[i].classList.remove("active");
+    }
+}
+
+function addActiveClass(num){
+    allSection[num].classList.add("active");
+}
 
 function showSection(element){
     for (let i=0; i<totalSection; i++){
@@ -35,15 +65,176 @@ function showSection(element){
 
 function updateNav(element){
     for (let i=0; i<totalNavList; i++){
-        navList[i].querySelector("a").classList.remove("active");
+        navList[i].classList.remove("active");
         const target=element.getAttribute("href").split("#")[1];
-        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
-            navList[i].querySelector("a").classList.add("active");
+        if (target === navList[i].getAttribute("href").split("#")[1]){
+            navList[i].classList.add("active");
         } 
     }
 }
-//
-//document.querySelector(".learn-more").addEventListener("click",function(){
+
+document.querySelector(".learn-more").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+})
+
+document.querySelector(".check-out").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+})
+
+document.querySelector(".check-out2").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+})
+
+document.querySelector(".contact-me").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+})
+
+document.querySelector(".contact-me2").addEventListener("click",function(){
+    const sectionIndex=this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cursor = document.querySelector('.cursor');
+
+const editCursor = e => {
+    const { clientX: x, clientY: y } = e;
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+};
+
+window.addEventListener('mousemove', editCursor);
+
+
+
+
+const links = document.querySelectorAll('a, .hover-this')
+
+links.forEach(link => {
+    link.addEventListener('mouseenter', e => {
+        cursor.classList.add('enlarged')
+    })
+    link.addEventListener('mouseout', e => {
+        cursor.classList.remove('enlarged')
+    })
+});
+
+
+
+
+$(document).ready(function(){
+    $('.accordion-list > li > .answer').hide();
+    $('.imageContainer > li > img').hide();
+
+    
+    $('.accordion-list > li').click(function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active").find(".answer").slideUp();
+        } else {
+            $(".accordion-list > li.active .answer").slideUp();
+            $(".accordion-list > li.active").removeClass("active");
+            $(this).addClass("active").find(".answer").slideDown();
+        }
+
+        return false;
+    });
+    
+    
+    $('.accordion-list > .project1').click(function() {
+        if ($(this).hasClass("active")) {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").show();
+            
+        } else {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").hide();
+        }
+
+        return false;
+        
+        
+    });
+    
+    $('.accordion-list > .project2').click(function() {
+        if ($(this).hasClass("active")) {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").show();
+            
+        } else {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").hide();
+        }
+
+        return false;
+        
+        
+    });
+    
+    $('.accordion-list > .project3').click(function() {
+        if ($(this).hasClass("active")) {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").show();
+            
+        } else {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").hide();
+        }
+
+        return false;
+        
+        
+    });
+    
+    $('.accordion-list > .project4').click(function() {
+        if ($(this).hasClass("active")) {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").show();
+            
+        } else {
+            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").hide();
+        }
+
+        return false;
+        
+        
+    });
+    
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //    const sectionIndex=this.getAttribute("data-section-index");
 //    showSection(this);
 //    updateNav(this);
