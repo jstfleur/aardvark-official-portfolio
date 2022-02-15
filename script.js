472b194a91aa3004d9c57778d67bf24b992c7be6
jstfleur <32300873+jstfleur@users.noreply.github.com>
2022-02-15 01:47:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -118,156 +118,6 @@ document.querySelector(".contact-me2").addEventListener("click",function(){
 
 
 
-cursor = document.querySelector('.cursor');
-
-const editCursor = e => {
-    const { clientX: x, clientY: y } = e;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-};
-
-window.addEventListener('mousemove', editCursor);
-
-
-
-
-const links = document.querySelectorAll('a, .hover-this')
-
-links.forEach(link => {
-    link.addEventListener('mouseenter', e => {
-        cursor.classList.add('enlarged')
-    })
-    link.addEventListener('mouseout', e => {
-        cursor.classList.remove('enlarged')
-    })
-});
-
-
-
-
-$(document).ready(function(){
-    $('.accordion-list > li > .answer').hide();
-    $('.imageContainer > li > img').hide();
-
-    
-    $('.accordion-list > li').click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).removeClass("active").find(".answer").slideUp();
-        } else {
-            $(".accordion-list > li.active .answer").slideUp();
-            $(".accordion-list > li.active").removeClass("active");
-            $(this).addClass("active").find(".answer").slideDown();
-        }
-
-        return false;
-    });
-    
-    
-    $('.accordion-list > .project1').click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").show();
-            
-        } else {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project1 img").hide();
-        }
-
-        return false;
-        
-        
-    });
-    
-    $('.accordion-list > .project2').click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").show();
-            
-        } else {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project2 img").hide();
-        }
-
-        return false;
-        
-        
-    });
-    
-    $('.accordion-list > .project3').click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").show();
-            
-        } else {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project3 img").hide();
-        }
-
-        return false;
-        
-        
-    });
-    
-    $('.accordion-list > .project4').click(function() {
-        if ($(this).hasClass("active")) {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li img").hide();
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").show();
-            
-        } else {
-            $(this).parent(".accordion-list").siblings(".imageContainer").find("li.project4 img").hide();
-        }
-
-        return false;
-        
-        
-    });
-    
-  
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    const sectionIndex=this.getAttribute("data-section-index");
-//    showSection(this);
-//    updateNav(this);
-//})
-//
-document.querySelector(".check-out").addEventListener("click",function(){
-    const sectionIndex=this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-})
-
-document.querySelector(".check-out2").addEventListener("click",function(){
-    const sectionIndex=this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-})
-
-document.querySelector(".contact-me").addEventListener("click",function(){
-    const sectionIndex=this.getAttribute("data-section-index");
-    showSection(this);
-    updateNav(this);
-})
-
-
-
-
-
-
-
-
-
-
-
 const cursor = document.querySelector('.cursor');
 
 const editCursor = e => {
@@ -371,3 +221,16 @@ $(document).ready(function(){
     
   
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
